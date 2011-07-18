6a71f3abd02d4fc282aef4e7794961372c95b7b0
Jonas Wielicki <j.wielicki@sotecware.net>
2011-07-18 16:19:43 +0200

    * Fixed wrong setHeader call

--- priyomhttp/server/servlets/empty.py
+++ priyomhttp/server/servlets/empty.py
@@ -4,7 +4,7 @@ from ..servlets import register
 
 class EmptyServlet(baseServlet.Servlet):
 	def do_GET(self, pathSegments, arguments, rfile, wfile):
-		self.setHeader("Content-Type: text/html");
+		self.setHeader("Content-Type", "text/html");
 		wfile.write("<html><head><title>api.priyom.org</title></head><body><h1>Welcome!</h1><p>Welcome to the <a href=\"http://priyom.org\">priyom.org</a> api server.</p></body></html>");
 
 
