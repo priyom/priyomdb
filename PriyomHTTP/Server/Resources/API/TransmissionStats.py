828cf2ccb62784a63b3cb3da856cbba55d0fc375
Jonas Wielicki <j.wielicki@sotecware.net>
2011-09-09 17:10:09 +0200

    * Fixed UnboundLocalError in TransmissionStatsAPI

--- PriyomHTTP/Server/Resources/API/TransmissionStats.py
+++ PriyomHTTP/Server/Resources/API/TransmissionStats.py
@@ -9,9 +9,9 @@ class TransmissionStatsAPI(API):
         super(TransmissionStatsAPI, self).handle(trans)
         stationId = self.getQueryInt("stationId", "must be integer")
         
+        lastModified, months = self.priyomInterface.getTransmissionStats(stationId, notModifiedCheck=self.autoNotModified, head=self.head)
         trans.set_content_type(ContentType("application/xml"))
         trans.set_header_value('Last-Modified', self.model.formatHTTPTimestamp(float(lastModified)))
-        lastModified, months = self.priyomInterface.getTransmissionStats(stationId, notModifiedCheck=self.autoNotModified, head=self.head)
         if self.head:
             return
         
