58e648101967ccc42b5cda5a9766aea86e01a586
Jonas Wielicki <j.wielicki@sotecware.net>
2011-09-09 16:30:31 +0200

    * Implemented parsing of HTTP dates

--- PriyomHTTP/Server/WebModel.py
+++ PriyomHTTP/Server/WebModel.py
@@ -5,9 +5,49 @@ import json
 from datetime import datetime, timedelta
 from libPriyom import Transmission, Station, Broadcast, Schedule
 from APIDatabase import Variable
+import re
 
 weekdayname = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 monthname = [None, 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+monthname_to_idx = {
+    'Jan': 1,
+    'Feb': 2,
+    'Mar': 3,
+    'Apr': 4,
+    'May': 5,
+    'Jun': 6,
+    'Jul': 7,
+    'Aug': 8,
+    'Sep': 9,
+    'Oct': 10,
+    'Nov': 11,
+    'Dec': 12
+}
+rfc1123 = {
+    "expression": re.compile("[A-Za-z]{3},\s+([0-9]{2})\s+([A-Za-z]{3})\s+([0-9]{4})\s+([0-9]{2}:[0-9]{2}:[0-9]{2})\s+(GMT|UTC)"),
+    "year": 2,
+    "month": 1,
+    "day": 0,
+    "time": 3,
+    "yearmode": "%Y"
+}
+rfc850 = {
+    "expression": re.compile("[A-Za-z]+,\s+([0-9]{2})-([A-Za-z]{3})-([0-9]{2})\s+([0-9]{2}:[0-9]{2}:[0-9]{2})\s+(GMT|UTC)"),
+    "year": 2,
+    "month": 1,
+    "day": 0,
+    "time": 3,
+    "yearmode": "%y"
+}
+asctime = {
+    "expression": re.compile("[A-Za-z]{3}\s+([A-Za-z]{3})\s+([0-9]{1,2})\s+([0-9]{2}:[0-9]{2}:[0-9]{2})\s+([0-9]{4})"),
+    "year": 3,
+    "month": 0,
+    "day": 1,
+    "time": 2,
+    "yearmode": "%Y"
+}
+
 
 class WebModel(object):
     def __init__(self, priyomInterface):
@@ -74,6 +114,30 @@ class WebModel(object):
         
     def formatHTTPTimestamp(self, timestamp):
         return self.formatHTTPDate(datetime.fromtimestamp(timestamp))
+        
+    def parseHTTPTimestamp(self, httpDate):
+        global rfc1123, rfc850, asctime, monthname_to_idx
+        standard = rfc1123
+        match = standard["expression"].match(httpDate)
+        if match is None:
+            standard = rfc850
+            match = standard["expression"].match(httpDate)
+        if match is None:
+            standard = asctime
+            match = standard["expression"].match(httpDate)
+        if match is None:
+            return None
+        
+        groups = match.groups()
+        year = groups[standard["year"]]
+        try:
+            month = monthname_to_idx[groups[standard["month"]]]
+        except KeyError:
+            return None
+        day = groups[standard["day"]]
+        time = groups[standard["time"]]
+        
+        return datetime.strptime("%s %d %s %s" % (year, month, day, time), "%s %%m %%d %%H:%%M:%%S" % (standard["yearmode"]))
         
     def now(self):
         return self.priyomInterface.now()
