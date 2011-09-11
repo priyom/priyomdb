13e55c9bf4c6e6ff6ae43cc9ddc1d907e7414bb9
Jonas Wielicki <j.wielicki@sotecware.net>
2011-09-11 18:29:48 +0200

    * Properly define allowedMethods set

--- PriyomHTTP/Server/Resources/Resource.py
+++ PriyomHTTP/Server/Resources/Resource.py
@@ -1,7 +1,7 @@
 from WebStack.Generic import EndOfResponse
 
 class Resource(object):
-    allowedMethods = set("HEAD", "GET")
+    allowedMethods = set(("HEAD", "GET"))
     
     def __init__(self, model):
         self.model = model
