09ae6a9a9c1f7e04d7cdb8a8f3631884049dfc3b
Jonas Wielicki <j.wielicki@sotecware.net>
2011-09-21 14:57:43 +0200

    * Fixed frozenset initialization

--- PriyomHTTP/Server/Resources/API/InstanciateSchedules.py
+++ PriyomHTTP/Server/Resources/API/InstanciateSchedules.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 class InstanciateSchedulesAPI(API):
     def __init__(self, model):
         super(InstanciateSchedulesAPI, self).__init__(model)
-        self.allowedMethods = frozenset("POST", "GET", "HEAD")
+        self.allowedMethods = frozenset(("POST", "GET", "HEAD"))
     
     def handle(self, trans):
         super(InstanciateSchedulesAPI, self).handle(trans)
@@ -18,7 +18,7 @@ class InstanciateSchedulesAPI(API):
         if self.head:
             return
         if trans.get_request_method() == "GET":
-            print >>self.out, u"Call this resource with POST to perform instanciation."
+            print >>self.out, u"Call this resource with POST to perform instanciation.".encode(self.encoding)
             return
         
         generatedUntil = 0
@@ -27,5 +27,5 @@ class InstanciateSchedulesAPI(API):
         else:
             generatedUntil = self.priyomInterface.scheduleMaintainer.updateSchedule(self.store.get(Station, stationId), None)
         
-        print >>self.out, u"Schedules generated until {0}".format(datetime.fromtimestamp(self.priyomInterface.now()))
+        print >>self.out, u"Schedules generated until {0}".format(datetime.fromtimestamp(self.priyomInterface.now())).encode(self.encoding)
 
