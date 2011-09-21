a0962015da34d4623c68bbec4eb8076544f8e1a2
Jonas Wielicki <j.wielicki@sotecware.net>
2011-09-21 15:48:01 +0200

    * Implemented handling of Documentation requests

--- PriyomHTTP/Server/Selectors/Documentation.py
+++ PriyomHTTP/Server/Selectors/Documentation.py
@@ -1,7 +1,14 @@
+from WebStack.Generic import ContentType
 
 class DocumentationSelector(object):
     def __init__(self, resource):
         self.resource = resource
     
     def respond(self, trans):
-        trans.set_response_code(500)
+        if hasattr(self.resource, "doc"):
+            return self.resource.doc(trans)
+        else:
+            trans.rollback()
+            trans.set_response_code(501)
+            trans.set_content_type(ContentType("text/plain", "utf-8"))
+            print >>trans.get_response_stream(), u"Documentation not implemented.".encode("utf-8")
