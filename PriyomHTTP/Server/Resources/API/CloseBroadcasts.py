2d79390c50f183158f1a8f894bc0bab570becb34
Jonas Wielicki <j.wielicki@sotecware.net>
2011-09-21 17:16:27 +0200

    * Documented CloseBroadcasts API call

--- PriyomHTTP/Server/Resources/API/CloseBroadcasts.py
+++ PriyomHTTP/Server/Resources/API/CloseBroadcasts.py
@@ -1,8 +1,18 @@
 from WebStack.Generic import ContentType
 from libPriyom import *
-from API import API
+from API import API, CallSyntax, Argument
 
 class CloseBroadcastsAPI(API):
+    title = u"getCloseBroadcasts"
+    shortDescription = u"Find broadcasts close to a given timestamp"
+    
+    docArgs = [
+        Argument(u"stationId", u"station ID", u"id of the station at which to search for broadcasts", metavar=u"stationid"),
+        Argument(u"time", u"unix timestamp", u"center of the time area which to take into account", metavar=u"timestamp"),
+        Argument(u"jitter", u"seconds", u"amount of seconds to look around the given timestamp. defaults to 600 seconds", metavar=u"seconds", optional=True)
+    ]
+    docCallSyntax = CallSyntax(docArgs, "?{0}&{1}&{2}")
+    
     def handle(self, trans):
         stationId = self.getQueryInt("stationId", "int; id of the station")
         time = self.getQueryInt("time", "int; unix timestamp at which to look")
