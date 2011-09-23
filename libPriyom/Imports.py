09b1f5256fe7ea603bdc126e8fbccb0a50b93641
Jonas Wielicki <j.wielicki@sotecware.net>
2011-09-24 01:30:27 +0200

    * Only call if it actually exists

--- libPriyom/Imports.py
+++ libPriyom/Imports.py
@@ -28,7 +28,8 @@ class ImportContext(object):
         if id < 0:
             obj = cls()
             self.store.add(obj)
-            obj.__storm_loaded__()
+            if hasattr(obj, "__storm_loaded__"):
+                obj.__storm_loaded__()
         else:
             obj = self.store.get(cls, id)
             if obj is None:
