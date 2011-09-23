396922dddb923d2a358010e37acd5c2f35bef2fd
Jonas Wielicki <j.wielicki@sotecware.net>
2011-09-23 19:38:11 +0200

    * Added SubmitLog resource

---
+++ PriyomHTTP/Server/Resources/SubmitLog.py
@@ -0,0 +1,20 @@
+from WebStack.Generic import ContentType, EndOfResponse
+from Resource import Resource
+from libPriyom import *
+
+class SubmitLogResource(object):
+    def __init__(self, model):
+        super(SubmitLogResource, self).__init__(model)
+        self.allowedMethods = frozenset(["GET", "POST"])
+    
+    def handle(self, trans):
+        trans.set_content_type(ContentType("text/html", self.encoding))
+        
+        print >>self.out, u"""<html>
+    <head>
+        <title>Submit logs</title>
+    </head>
+    <body>
+    
+    </body>
+</html>""".encode(self.encoding)
