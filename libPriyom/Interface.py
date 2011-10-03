8d1833446249f25c9a9650f8b1d784b264a89246
Jonas Wielicki <j.wielicki@sotecware.net>
2011-10-03 13:36:48 +0200

    * Adapted other files to new Transmission class names

--- libPriyom/Interface.py
+++ libPriyom/Interface.py
@@ -31,7 +31,7 @@ import XMLIntf
 import xml.dom.minidom as dom
 from Modulation import Modulation
 from Broadcast import BroadcastFrequency, Broadcast
-from Transmission import Transmission, TransmissionClass, TransmissionClassTable, TransmissionClassTableField
+from Transmission import Transmission, TransmissionClass, TransmissionTable, TransmissionTableField
 from Schedule import Schedule, ScheduleLeaf
 from Station import Station
 from Foreign import ForeignSupplement
@@ -61,7 +61,7 @@ class PriyomInterface:
         Station: "priyom-station-export",
         Broadcast: "priyom-broadcast-export",
         TransmissionClass: "priyom-generic-export",
-        TransmissionClassTable: "priyom-generic-export"
+        TransmissionTable: "priyom-generic-export"
     }
     
     def __init__(self, store):
