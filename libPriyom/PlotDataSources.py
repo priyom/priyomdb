788d0a43d92a1fd1a257992ff96d02a7be0e90b3
Jonas Wielicki <j.wielicki@sotecware.net>
2011-10-06 12:28:27 +0200

    * A lot of refinement on the uptime graphs

--- libPriyom/PlotDataSources.py
+++ libPriyom/PlotDataSources.py
@@ -3,6 +3,8 @@ from storm.expr import *
 from Transmission import Transmission
 from Broadcast import Broadcast
 from Station import Station
+from Helpers import TimeUtils
+import itertools
 
 __all__ = ['PlotDataSource', 'PlotDataPunch', 'PlotDataWeekHourPunch', 'PlotDataMonthHourPunch', 'PlotDataUptime']
 
@@ -17,11 +19,12 @@ class PlotDataSource(object):
         pass
         
 class PlotDataUptime(PlotDataSource):
-    height = 6
+    height = 7
     width = 12.5
     
-    xborder = 0.07
-    yborder_bottom = 0.15
+    xborder_left = 0.07
+    xborder_right = 0.01
+    yborder_bottom = 0.14
     yborder_top = 0.03
     
     def getLastModified(self, stationId = None, **kwargs):
@@ -37,46 +40,137 @@ class PlotDataUptime(PlotDataSource):
                             Broadcast.BroadcastEnd != None).max(Broadcast.Modified),
                         self.store.get(Station, stationId).BroadcastRemoved,
                         self.store.get(Station, stationId).Modified)
+            
+    def _splitData(self, data):
+        prevStation = None
+        l = None
+        for item in data:
+            if prevStation != item[0]:
+                if l is not None:
+                    yield (prevStation, l)
+                l = list()
+                prevStation = item[0]
+            l.append(item[1:])
+            
+    def _getMonthList(self, data):
+        mapping = dict()
+        prevMonth = None
+        for stationItem in data:
+            for monthItem in stationItem[1]:
+                t = (monthItem[0], monthItem[1]-1)
+                if not t in mapping:
+                    mapping[t] = None
+        
+        keys = [(t, float(t[0])+float(t[1])/12.) for t in mapping.iterkeys()]
+        keys.sort(key=lambda x: x[1])
+        for t, v in keys:
+            if prevMonth is not None:
+                for year in xrange(prevMonth[0], t[0]+1):
+                    minMonth = 0 if year > prevMonth[0] else prevMonth[1]+1
+                    maxMonth = 12 if year < t[0] else t[1]
+                    if minMonth == 12:
+                        continue
+                    if maxMonth < 0:
+                        continue
+                    for month in xrange(minMonth, maxMonth):
+                        yield (year, month)
+            yield t
+            prevMonth = t
+        
+    def _mapData(self, data, monthList):
+        monthList = [((year, month), 0) for year, month in monthList]
+        
+        for stationItem in data:
+            station = stationItem[0]
+            newStationData = dict(monthList)
+            for monthItem in stationItem[1]:
+                t = (monthItem[0], monthItem[1]-1)
+                newStationData[t] = float(monthItem[2])
+            yield (station, [(year, month, duration) for (year, month), duration in sorted(newStationData.iteritems(), key=lambda x: float(x[0][0])+float(x[0][1])/12.)])
+            
+    def _filterTicksExt(self, list, reference):
+        yield list[0]
+        for item, ref in itertools.izip(list[1:-1], reference[1:-1]):
+            month = ref[1]
+            if not month in (0, 3, 6, 9):
+                continue
+            yield item
+        yield list[-1]
                         
-    def getData(self, stationId = None, **kwargs):
+    def getData(self, stationId = None, 
+            years=None,
+            **kwargs):
         if stationId == 0:
             stationId = None
-        if stationId is None:
-            data = self.store.find(
-                (
-                    Station,
-                    Func("YEAR", Func("FROM_UNIXTIME", Broadcast.BroadcastStart)), 
-                    Func("MONTH", Func("FROM_UNIXTIME", Broadcast.BroadcastStart)),
-                    Func("SUM", Broadcast.BroadcastEnd - Broadcast.BroadcastStart)
-                ), 
-                Broadcast.StationID == Station.ID,
-                Broadcast.BroadcastEnd != None
-            ).group_by(Broadcast.StationID, Func("YEAR", Func("FROM_UNIXTIME", Broadcast.BroadcastStart)), Func("MONTH", Func("FROM_UNIXTIME", Broadcast.BroadcastStart)))
-        else:
-            data = self.store.find(
-                (
-                    Station,
-                    Func("YEAR", Func("FROM_UNIXTIME", Broadcast.BroadcastStart)), 
-                    Func("MONTH", Func("FROM_UNIXTIME", Broadcast.BroadcastStart)),
-                    Func("SUM", Broadcast.BroadcastEnd - Broadcast.BroadcastStart)
-                ), 
-                Broadcast.StationID == Station.ID,
-                Broadcast.StationID == stationId,
-                Broadcast.BroadcastEnd != None
-            ).group_by(Func("YEAR", Func("FROM_UNIXTIME", Broadcast.BroadcastStart)), Func("MONTH", Func("FROM_UNIXTIME", Broadcast.BroadcastStart)))
-        return data
+        where = Broadcast.BroadcastEnd != None
+        if stationId is not None:
+            where = And(where, Broadcast.StationID == stationId)
+        if years is not None:
+            now = TimeUtils.nowDate()
+            where = And(where, Broadcast.BroadcastStart >= TimeUtils.toTimestamp(datetime(year=now.year-years, month=now.month, day=1)))
+        data = self.store.find(
+            (
+                Station,
+                Func("YEAR", Func("FROM_UNIXTIME", Broadcast.BroadcastStart)), 
+                Func("MONTH", Func("FROM_UNIXTIME", Broadcast.BroadcastStart)),
+                Func("SUM", Broadcast.BroadcastEnd - Broadcast.BroadcastStart)
+            ), 
+            Broadcast.StationID == Station.ID,
+            where
+        ).group_by(Station, Func("YEAR", Func("FROM_UNIXTIME", Broadcast.BroadcastStart)), Func("MONTH", Func("FROM_UNIXTIME", Broadcast.BroadcastStart)))
         
-    def setupAxes(self, figure,
+        data = list(self._splitData(data))
+        monthList = list(self._getMonthList(data))
+        data = list(self._mapData(data, monthList))
+        
+        dateList = [datetime(year, month+1, day=1, hour=0, minute=0) for year, month in monthList]
+        xticks = list(self._filterTicksExt(dateList, monthList))
+        tickCoords = [float(i)+0.5 for i in self._filterTicksExt(range(len(monthList)), monthList)]
+        coords = [i for i in xrange(len(monthList))]
+        
+        return (data, coords, xticks, tickCoords)
+        
+    def setupAxes(self, figure, dataTuple,
         fontProp = None,
         axisColour=(0.3, 0.3, 0.3, 1.0),
         labelColour=(0., 0., 0., 1.0), 
+        gridColour=(0.5, 0.5, 0.5, 1.),
+        title=None,
+        years=None,
         **kwargs):
         
-        figure.set_figheight(self.height)
-        figure.set_figwidth(self.width)
+        width = self.width if years is None else years*2
+        if width > self.width:
+            width = width / 2.
+        if width < 5:
+            width = 5
+        xborder_left = self.width / width * self.xborder_left
+        xborder_right = self.width / width * self.xborder_right
         
-        ax = figure.add_axes([self.xborder, self.yborder_bottom, 1.0-self.xborder, 1.0-(self.yborder_top+self.yborder_bottom)])
+        data, coords, xticks, tickCoords = dataTuple
+        
+        figure.set_figheight(self.height)
+        figure.set_figwidth(width)
+        
+        ax = figure.add_axes([xborder_left, self.yborder_bottom, 1.0-(xborder_left+xborder_right), 1.0-(self.yborder_top+self.yborder_bottom)])
         ax.set_frame_on(False)
+        
+        ax.minorticks_on()
+        
+        ax.set_xticks(tickCoords)
+        ax.set_xticks([coord+0.5 for coord in coords[::2]], minor=True)
+        ax.set_xticklabels([u"{0} {1:d}".format(TimeUtils.monthname[dt.month-1], dt.year) for dt in xticks], rotation="vertical", font_properties=fontProp, color=labelColour)
+        ax.set_ylabel("seconds of transmission", font_properties=fontProp)
+        ax.set_xlabel("month", font_properties=fontProp)
+        ax.grid(True, which='both', linestyle=':', color=gridColour)
+        ax.tick_params(direction="out", color=axisColour)
+        
+        for tick in ax.yaxis.get_major_ticks():
+            tick.label1.set_font_properties(fontProp)
+        
+        ax.set_title(title or u"transmission duration per month by station", font_properties=fontProp)
+        ax.axhline(linewidth=2., color=axisColour)
+        ax.axvline(linewidth=2., color=axisColour)
         
         ax.tick_params(color=axisColour)
         
@@ -192,7 +286,7 @@ class PlotDataMonthHourPunch(PlotDataPunch):
     ycount = 12
     
     xtickLabels = PlotDataWeekHourPunch.xtickLabels
-    ytickLabels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Oct", "Sep", "Nov", "Dec"]
+    ytickLabels = TimeUtils.monthname
     
     def select(self, stationId = None, **kwargs):
         cols = (
