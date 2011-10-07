ec924df2182acc5d4cd1c883c43791efb12e2fe3
Jonas Wielicki <j.wielicki@sotecware.net>
2011-10-07 21:22:26 +0200

    * Added insertion procedure to SubmitEventResource

--- PriyomHTTP/Server/Resources/SubmitEvent.py
+++ PriyomHTTP/Server/Resources/SubmitEvent.py
@@ -39,20 +39,61 @@ class SubmitEventResource(Resource):
         super(SubmitEventResource, self).__init__(model)
         self.allowedMethods = frozenset(("GET", "POST"))
         
+        self.startTimeTypecast = self.model.PriyomTimestamp()
+        self.endTimeTypecast = self.model.AllowBoth(
+            self.model.PriyomTimestamp(),
+            self.model.EmptyString()
+        )
+        
     def insert(self):
-        return False
+        try:
+            station = self.model.validStormObject(Station, self.store)(self.query["stationId"])
+            startTime = self.startTimeTypecast(self.query["startTime"])
+            endTime = self.endTimeTypecast(self.query["endTime"])
+            if endTime == u"":
+                endTime = None
+            description = self.query["description"]
+            if len(description) < 10:
+                raise ValueError(u"At least 10 characters of description required")
+            eventClass = self.model.validStormObject(EventClass, self.store)(self.query["eventClassId"]) if self.query["eventClassId"] != u"0" else None
+        except ValueError as e:
+            return unicode(e)
+        except KeyError as e:
+            return u"missing submit data: {0}".format(unicode(e))
+        except TypeError as e:
+            return unicode(e)
+            
+        event = Event()
+        try:
+            event.Station = station
+            event.StartTime = TimeUtils.toTimestamp(startTime)
+            event.EndTime = TimeUtils.toTimestamp(endTime) if endTime is not None else None
+            event.Description = description
+            event.EventClass = eventClass
+        except BaseException as e:
+            del event
+            return unicode(e)
+        self.store.add(event)
+        
+        print >>self.out, u"""<pre>Added new event to station {0}: {1}</pre>""".format(escape(unicode(station)), escape(unicode(event)))
     
     def handle(self, trans):
         trans.set_content_type(ContentType(self.xhtmlContentType, self.encoding))
         
         error = u""
         
-        station = self.store.get(Station, self.getQueryValue("stationId", int, default=0))
-        startTime = self.getQueryValue("startTime", self.model.PriyomTimestamp(), default=TimeUtils.nowDate())
-        endTime = self.getQueryValue("endTime", self.model.AllowBoth(self.model.PriyomTimestamp(allowNone=True), self.model.EmptyString()), defaultKey=u"")
+        station = self.getQueryValue("stationId", self.model.validStormObject(Station, self.store), default=None)
+        startTime = self.getQueryValue("startTime", self.model.PriyomTimestamp(), defaultKey=TimeUtils.nowDate(), default=None)
+        endTime = self.getQueryValue("endTime", self.model.AllowBoth(self.model.PriyomTimestamp(), self.model.EmptyString()), defaultKey=u"", default=None)
+        description = self.query.get("description", u"")
+        
+        if startTime is None:
+            error = u"Malformatted start time"
+            startTime = TimeUtils.nowDate()
+        if endTime is None:
+            error = u"Malformatted end time"
         if endTime == u"":
             endTime = None
-        description = self.query.get("description", u"")
         
         print >>self.out, u"""<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -67,13 +108,13 @@ class SubmitEventResource(Resource):
         ).encode(self.encoding, 'replace')
         
         submitted = False
-        if "submit" in self.query:
+        if "submit" in self.query and len(error) == 0:
             insertResult = self.insert()
             if insertResult is not None:
                 error = insertResult
             else:
                 submitted = True
-                
+        
         if not submitted:
             print >>self.out, u"""
         <form name="logform" method="POST">
@@ -93,12 +134,12 @@ class SubmitEventResource(Resource):
             </div>
             <div class="section">
                 <div class="inner-caption">Description</div>
-                <textarea rows="5">{5}</textarea>
+                <textarea rows="7" cols="100" name="description">{5}</textarea>
             </div>
             <input type="submit" name="submit" value="Submit" />
         </form>""".format(
             SubmitLogResource.recursiveDict(self.query),
-            u"" if len(error) == 0 else u"""<div class="error">{0}""".format(error),
+            u"" if len(error) == 0 else u"""<div class="error">{0}</div>""".format(escape(error)),
             u"\n                    ".join(
                 (u'<option value="{0}"{1}>{2}</option>'.format(tmpstation.ID, u' selected="selected"' if tmpstation is station else u'', escape(unicode(tmpstation))) for tmpstation in self.store.find(Station))
             ),
@@ -114,7 +155,8 @@ class SubmitEventResource(Resource):
         <div class="section">
             <div class="inner-caption">Notes</div>
             <ol>
-                <li><p>Note that for events which indicate state changes, there is no need to create two separate events to indicate start and ending. If the event class is “state changing” (its indicated in its name if so), two separate events will automatically be generated in any event listing output. These will be prefixed with “Start of” and “End of” respectively.</p></li>
+                <li><p>Note that for events which indicate state changes, there is no need to create two separate events to indicate start and ending. If the event class is “state changing” (its indicated in its name if so), two separate events will automatically be generated in any event listing output. These will be prefixed with “Start of” and “End of” respectively.</p>
+                <p>If the endTime is omitted, “Start of” is left out on the first and only event.</p></li>
                 <li><p>A raw event is any event which does not fit in any event class. Try to keep the description structured though, in case we want to merge these events to a common event class later. Watch out for existing events having similar contents before submitting.</p></li>
             </ol>
         </div>
