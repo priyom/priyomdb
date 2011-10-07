priyom/priyomdb

commit 42d140892a51125611dba03a8513c3423e8ed73b

* Make recursive dict methods static in SubmitLogResource

PriyomHTTP/Server/Resources/SubmitLog.py
@@ -135,7 +135,8 @@ class SubmitLogResource(Resource):
             
         yield u"""</div></div>"""
         
-    def recursiveDictNode(self, dictionary, indent = u""):
+    @staticmethod
+    def recursiveDictNode(dictionary, indent = u""):
         for key, value in dictionary.iteritems():
             if type(value) == dict:
                 yield u"""{1}{0}: {2}""".format(key, indent, u"{")
@@ -144,8 +145,9 @@ class SubmitLogResource(Resource):
                 yield u"""{0}{1}""".format(indent, u"}")
             else:
                 yield u"""{2}{0}: {1}""".format(key, repr(value), indent)
-        
-    def recursiveDict(self, dict):
+    
+    @staticmethod
+    def recursiveDict(dict):
         return "\n".join(self.recursiveDictNode(dict))
         
     def formatTransmissionEditor(self):

commit 016952ab224bbceac1de1f7e35a56d3b9c76a85c

* Fix regression introduced with 42d140892a51125611dba03a8513c3423e8ed73b

PriyomHTTP/Server/Resources/SubmitLog.py
@@ -140,7 +140,7 @@ class SubmitLogResource(Resource):
         for key, value in dictionary.iteritems():
             if type(value) == dict:
                 yield u"""{1}{0}: {2}""".format(key, indent, u"{")
-                for line in self.recursiveDictNode(value, indent + u"    "):
+                for line in SubmitLogResource.recursiveDictNode(value, indent + u"    "):
                     yield line
                 yield u"""{0}{1}""".format(indent, u"}")
             else:
@@ -148,7 +148,7 @@ class SubmitLogResource(Resource):
     
     @staticmethod
     def recursiveDict(dict):
-        return "\n".join(self.recursiveDictNode(dict))
+        return "\n".join(SubmitLogResource.recursiveDictNode(dict))
         
     def formatTransmissionEditor(self):
         yield u"""Transmission class: <select name="transmissionClass">"""