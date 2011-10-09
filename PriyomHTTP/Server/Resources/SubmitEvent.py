1c9d42cdceeb99dfebc48bb249820e2ff7e7d849
Jonas Wielicki <j.wielicki@sotecware.net>
2011-10-09 23:22:37 +0200

    * Adapted SubmitEvent to new SubmitResource

--- PriyomHTTP/Server/Resources/SubmitEvent.py
+++ PriyomHTTP/Server/Resources/SubmitEvent.py
@@ -27,14 +27,13 @@ authors:
 """
 from storm.locals import *
 from WebStack.Generic import ContentType, EndOfResponse
-from Resource import Resource
 from libPriyom import *
 from datetime import datetime, timedelta
-from .SubmitLog import SubmitLogResource
+from SubmitResource import SubmitResource, SubmitParameterError
 from xml.sax.saxutils import escape
 
 
-class SubmitEventResource(Resource):
+class SubmitEventResource(SubmitResource):
     def __init__(self, model):
         super(SubmitEventResource, self).__init__(model)
         self.allowedMethods = frozenset(("GET", "POST"))
@@ -45,121 +44,105 @@ class SubmitEventResource(Resource):
             self.model.EmptyString()
         )
         
+    def _eventInformationTree(self, parent):
+        parent[0].tail = u"Station: "
+        self._stationSelect(parent, name=u"station", value=self.station)
+        
+        self.br(parent, u"Event class: ")
+        eventClassSelect = self.SubElement(parent, u"select", name=u"eventClass")
+        self.SubElement(eventClassSelect, u"option", value=u"").text = u"Raw event (see note 2)"
+        
+        for eventClass in self.store.find(EventClass):
+            option = self.SubElement(eventClassSelect, u"option", value=unicode(eventClass.ID))
+            option.text = unicode(eventClass)
+            if eventClass is self.eventClass:
+                option.set(u"selected", u"selected")
+        
+        self.br(parent, u"Start time: ")
+        self.input(parent, name=u"startTime", value=self.startTime.strftime(Formatting.priyomdate))
+        
+        self.br(parent, u"End time: ")
+        self.input(parent, name=u"endTime", value=self.endTime.strftime(Formatting.priyomdate) if self.endTime is not None else u"").tail = u" (leave blank if this event is a single point in the time axis; see also the note 1 below)"
+        
+    def _descriptionTree(self, parent):
+        self.SubElement(parent, u"textarea", rows=u"7", name=u"description").text = self.description
+        
+    def _noteTree(self, parent):
+        ol = self.SubElement(parent, u"ol")
+        li = self.SubElement(ol, u"li")
+        self.SubElement(li, u"p").text = u"Note that for events which indicate state changes, there is no need to create two separate events to indicate start and ending. If the event class is “state changing” (its indicated in its name if so), two separate events will automatically be generated in any event listing output. These will be prefixed with “Start of” and “End of” respectively."
+        self.SubElement(li, u"p").text = u"If the endTime is omitted, “Start of” is left out on the first and only event."
+        
+        li = self.SubElement(ol, u"li")
+        self.SubElement(li, u"p").text = u"A raw event is any event which does not fit in any event class. Try to keep the description structured though, in case we want to merge these events to a common event class later. Watch out for existing events having similar contents before submitting."
+        
     def insert(self):
-        try:
-            station = self.model.validStormObject(Station, self.store)(self.query["stationId"])
-            startTime = self.startTimeTypecast(self.query["startTime"])
-            endTime = self.endTimeTypecast(self.query["endTime"])
-            if endTime == u"":
-                endTime = None
-            description = self.query["description"]
-            if len(description) < 10:
-                raise ValueError(u"At least 10 characters of description required")
-            eventClass = self.model.validStormObject(EventClass, self.store)(self.query["eventClassId"]) if self.query["eventClassId"] != u"0" else None
-        except ValueError as e:
-            return unicode(e)
-        except KeyError as e:
-            return u"missing submit data: {0}".format(unicode(e))
-        except TypeError as e:
-            return unicode(e)
+        self.station = self.getQueryValue("station", self.model.validStormObject(Station, self.store))
+        
+        if self.error is not None:
+            self.setError(self.error)
             
         event = Event()
         try:
-            event.Station = station
-            event.StartTime = TimeUtils.toTimestamp(startTime)
-            event.EndTime = TimeUtils.toTimestamp(endTime) if endTime is not None else None
-            event.Description = description
-            event.EventClass = eventClass
+            event.Station = self.station
+            event.StartTime = TimeUtils.toTimestamp(self.startTime)
+            event.EndTime = TimeUtils.toTimestamp(self.endTime) if self.endTime is not None else None
+            event.Description = self.description
+            event.EventClass = self.eventClass
         except BaseException as e:
             del event
-            return unicode(e)
+            self.setError(unicode(e))
         self.store.add(event)
+        self.store.commit()
         
-        print >>self.out, u"""<pre>Added new event to station {0}: {1}</pre>""".format(escape(unicode(station)), escape(unicode(event)))
+        self.SubElement(self.body, u"pre").text = u"""Added new event
+Station: {0}
+Event (#{2}): {1}""".format(
+            unicode(event.Station),
+            unicode(event),
+            unicode(event.ID)
+        )
     
-    def handle(self, trans):
-        trans.set_content_type(ContentType(self.xhtmlContentType, self.encoding))
+    def buildDoc(self, trans, elements):
+        super(SubmitEventResource, self).buildDoc(trans, elements)
         
-        error = u""
+        self.station = self.getQueryValue("station", self.model.validStormObject(Station, self.store), defaultKey=None)
+        self.eventClass = self.getQueryValue("eventClass", self.model.AllowBoth(self.model.validStormObject(EventClass, self.store), self.model.EmptyString()), defaultKey=None)
+        if self.eventClass == u"":
+            self.eventClass = None
+        self.startTime = self.getQueryValue("startTime", self.model.PriyomTimestamp(), defaultKey=TimeUtils.nowDate())
+        self.endTime = self.getQueryValue("endTime", self.model.AllowBoth(self.model.PriyomTimestamp(), self.model.EmptyString()), defaultKey=None)
+        if self.endTime == u"":
+            self.endTime = None
+        self.description = self.query.get("description", u"")
+        if len(self.description) < 10:
+            self.error = u"Description must be at least 10 characters long."
         
-        station = self.getQueryValue("stationId", self.model.validStormObject(Station, self.store), default=None)
-        eventClass = self.getQueryValue("eventClassId", self.model.validStormObject(EventClass, self.store), default=None)
-        startTime = self.getQueryValue("startTime", self.model.PriyomTimestamp(), defaultKey=TimeUtils.nowDate(), default=None)
-        endTime = self.getQueryValue("endTime", self.model.AllowBoth(self.model.PriyomTimestamp(), self.model.EmptyString()), defaultKey=u"", default=None)
-        description = self.query.get("description", u"")
-        
-        if startTime is None:
-            error = u"Malformatted start time"
-            startTime = TimeUtils.nowDate()
-        if endTime is None:
-            error = u"Malformatted end time"
-        if endTime == u"":
-            endTime = None
-        
-        print >>self.out, u"""<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <title>{1}</title>
-        <link rel="stylesheet" type="text/css" href="{0}{2}" />
-    </head>
-    <body>""".format(
-            self.model.rootPath(u""),
-            self.model.formatHTMLTitle(u"Submit events"),
-            u"/css/submit.css",
-        ).encode(self.encoding, 'replace')
+        self.link(u"/css/submit.css")
+        self.setTitle(u"Submit event")
         
         submitted = False
-        if "submit" in self.query and len(error) == 0:
-            insertResult = self.insert()
-            if insertResult is not None:
-                error = insertResult
-            else:
+        if "submit" in self.query and self.error is None:
+            try:
+                self.insert()
                 submitted = True
+            except SubmitParameterError:
+                submitted = False
+            
         
         if not submitted:
-            print >>self.out, u"""
-        <form name="logform" method="POST">
-            <pre>{0}</pre>
-            {1}
-            <div class="section">
-                <div class="inner-caption">Event information</div>
-                Station: <select name="stationId">
-                    {2}
-                </select><br />
-                Event class: <select name="eventClassId">
-                    <option value="0">Raw event (see note 2 below)</option>
-                    {6}
-                </select><br />
-                Start time: <input type="text" name="startTime" value="{3}" /><br />
-                End time: <input type="text" name="endTime" value="{4}" /> (leave blank if this event is a single point in the time axis; see also the note 1 below)<br />
-            </div>
-            <div class="section">
-                <div class="inner-caption">Description</div>
-                <textarea rows="7" cols="100" name="description">{5}</textarea>
-            </div>
-            <input type="submit" name="submit" value="Submit" />
-        </form>""".format(
-            SubmitLogResource.recursiveDict(self.query),
-            u"" if len(error) == 0 else u"""<div class="error">{0}</div>""".format(escape(error)),
-            u"\n                    ".join(
-                (u'<option value="{0}"{1}>{2}</option>'.format(tmpstation.ID, u' selected="selected"' if tmpstation is station else u'', escape(unicode(tmpstation))) for tmpstation in self.store.find(Station))
-            ),
-            startTime.strftime(Formatting.priyomdate),
-            endTime.strftime(Formatting.priyomdate) if endTime is not None else u"",
-            escape(description),
-            u"\n                    ".join(
-                (u'<option value="{0}"{1}>{2}</option>'.format(tmpEventClass.ID, u' selected="selected"' if tmpEventClass is eventClass else u'', escape(unicode(tmpEventClass))) for tmpEventClass in self.store.find(EventClass))
-            )
-        ).encode(self.encoding, 'replace')
-        
-        print >>self.out, u"""
-        <div class="section">
-            <div class="inner-caption">Notes</div>
-            <ol>
-                <li><p>Note that for events which indicate state changes, there is no need to create two separate events to indicate start and ending. If the event class is “state changing” (its indicated in its name if so), two separate events will automatically be generated in any event listing output. These will be prefixed with “Start of” and “End of” respectively.</p>
-                <p>If the endTime is omitted, “Start of” is left out on the first and only event.</p></li>
-                <li><p>A raw event is any event which does not fit in any event class. Try to keep the description structured though, in case we want to merge these events to a common event class later. Watch out for existing events having similar contents before submitting.</p></li>
-            </ol>
-        </div>
-    </body>
-</html>""".encode(self.encoding, 'replace')
+            self.SubElement(self.body, u"pre").text = self.recursiveDict(self.query)
+            if self.error is not None:
+                self.SubElement(self.body, u"div", attrib={
+                    u"class": u"error"
+                }).text = self.error
+                
+            form = self.SubElement(self.body, u"form", name=u"logform", method=u"POST")
+            
+            self._eventInformationTree(self.section(form, u"Event information"))
+            self._descriptionTree(self.section(form, u"Description"))
+            
+            self.input(form, type=u"submit", name=u"submit", value=u"Submit")
+            
+            self._noteTree(self.section(self.body, u"Notes"))
+            
