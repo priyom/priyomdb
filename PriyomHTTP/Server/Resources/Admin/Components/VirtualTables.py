priyom/priyomdb

commit 04722ea05e66520dc6e05a8fce815ba3db37663d

* Cleaned up import syntax in PriyomHTTP/Server/Resources/Admin/Components/

PriyomHTTP/Server/Resources/Admin/Components/VirtualTables.py
@@ -28,11 +28,11 @@ authors:
 
 __all__ = ["VirtualColumn", "StormColumn", "ReferenceColumn", "IDColumn", "ModifiedColumn", "CreatedColumn", "TimestampColumn", "VirtualTable", "ReferencingTable", "Sortable", "Filterable"]
 
-from libPriyom import Formatting
 from storm.locals import *
 from storm.expr import *
 import itertools
-from libPriyom import Formatting
+
+import libPriyom.Formatting import Formatting
 from PriyomHTTP.Server.Resources.Admin.Components.Base import ParentComponent
 
 Sortable = 1

PriyomHTTP/Server/Resources/Admin/Components/Tables.py
@@ -28,10 +28,11 @@ authors:
 
 __all__ = ["TableComponent", "Table", "TableGroup", "IDTableGroup"]
 
-from PriyomHTTP.Server import HTMLIntf
+import itertools
+import PriyomHTTP.Server.HTMLIntf as HTMLIntf
 from PriyomHTTP.Server.Resources.Admin.Components.Base import Component, ParentComponent
 from PriyomHTTP.Server.Resources.Admin.Components.Editors import Input, Timestamp
-import itertools
+
 
 class TableComponent(Component):
     def toTableRow(self, tr):

PriyomHTTP/Server/Resources/Admin/Components/Base.py
@@ -27,7 +27,7 @@ authors:
 __all__ = ["Component", "EditorComponent", "ParentComponent"]
 
 import xml.etree.ElementTree as ElementTree
-from PriyomHTTP.Server import HTMLIntf
+import PriyomHTTP.Server.HTMLIntf as HTMLIntf
 
 class Component(object):
     def __init__(self, model=None, **kwargs):

PriyomHTTP/Server/Resources/Admin/Components/Editors.py
@@ -32,10 +32,12 @@ from storm.locals import *
 from storm.expr import *
 import itertools
 from Types import Typecasts
+
 import libPriyom
-from libPriyom import Formatting
-from libPriyom.Helpers import TimeUtils
-from PriyomHTTP.Server import HTMLIntf
+import libPriyom.Formatting as Formatting
+import libPriyom.Helpers.TimeUtils as TimeUtils
+
+import PriyomHTTP.Server.HTMLIntf as HTMLIntf
 from PriyomHTTP.Server.Resources.Admin.Components.Base import EditorComponent
 
 class Input(EditorComponent):

commit 75b4d14d15b2cfdb45088bbb7334538597f456f0

* Fixed regressions introduced by:   04722ea05e66520dc6e05a8fce815ba3db37663d   9d4e0a3756d998e183212010004f0a5f3d48b5f0

PriyomHTTP/Server/Resources/Admin/Components/VirtualTables.py
@@ -32,7 +32,7 @@ from storm.locals import *
 from storm.expr import *
 import itertools
 
-import libPriyom.Formatting import Formatting
+import libPriyom.Formatting as Formatting
 from PriyomHTTP.Server.Resources.Admin.Components.Base import ParentComponent
 
 Sortable = 1

PriyomHTTP/Server/Resources/Admin/Tables.py
@@ -31,7 +31,7 @@ import urllib
 
 import PriyomHTTP.Server.HTMLIntf as HTMLIntf
 from PriyomHTTP.Server.Resources.HTMLResource import HTMLResource
-import PriyomHTTP.Server.Resources.Admin.UITree as UITree
+import UITree as UITree
 from PriyomHTTP.Server.Resources.Admin.Components import Sortable, Filterable
 from Types import Typecasts
 