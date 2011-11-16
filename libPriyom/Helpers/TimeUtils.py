priyom/priyomdb

commit 2914bda21a4d31ec58cc3255775254721f700018

* New TimeUtils

libPriyom/Helpers/TimeUtils.py
@@ -40,6 +40,24 @@ def nowDate():
     
 def now():
     return toTimestamp(nowDate())
-    
+
+def nextMonth(dt):
+    if dt.month == 12:
+        return datetime(year=dt.year+1, month=1, 1)
+    else:
+        return datetime(year=dt.year, month=dt.month+1, 1)
+
+def monthTimeRange(year, month):
+    first = datetime(year, month, 1)
+    return (toTimestamp(first), toTimestamp(nextMonth(first)))
+
+def yearTimeRange(year):
+    return (toTimestamp(datetime(year, 1, 1)), toTimestamp(datetime(year+1, 1, 1)))
+
+def calendarTimeRange(year, month=None):
+    if month is not None:
+        return monthTimeRange(year, month)
+    else:
+        return yearTimeRange(year)
 
 fromTimestamp = toDatetime

commit 2fe232a3afceb74804b79f639b5768aa24d97d95

* Fixed non-keyword argument after keyword argument   This fixes a regression introduced with:   2914bda21a4d31ec58cc3255775254721f700018 * New TimeUtils

libPriyom/Helpers/TimeUtils.py
@@ -43,9 +43,9 @@ def now():
 
 def nextMonth(dt):
     if dt.month == 12:
-        return datetime(year=dt.year+1, month=1, 1)
+        return datetime(year=dt.year+1, month=1, day=1)
     else:
-        return datetime(year=dt.year, month=dt.month+1, 1)
+        return datetime(year=dt.year, month=dt.month+1, day=1)
 
 def monthTimeRange(year, month):
     first = datetime(year, month, 1)