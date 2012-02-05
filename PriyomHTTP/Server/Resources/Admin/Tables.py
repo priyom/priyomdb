priyom/priyomdb

commit 557a0e5cde689538b6fdd8057c88404fdaf4c59f

* Reworked column system again

PriyomHTTP/Server/Resources/Admin/Tables.py
@@ -104,53 +104,49 @@ class AdminTablesResource(HTMLResource):
         }).text = u"Act."
         HTMLIntf.SubElement(colgroup, u"col", span="1").set("style", "width: 8em;")
         
-        
         for column in virtualTable.columns:
             th = HTMLIntf.SubElement(thead, u"th")
+            a = HTMLIntf.SubElement(th, u"a")
+            order = HTMLIntf.SubElement(a, u"div", attrib={
+                u"class": u"order"
+            })
+            order.tail = column.title
+            
+            if orderColumn is not column:
+                a.set(u"href", self.buildQueryOnSameTable(orderColumn=column.name, orderDirection=column.defaultSort))
+            else:
+                if orderDirection == u"ASC":
+                    a.set(u"href", self.buildQueryOnSameTable(orderColumn=column.name, orderDirection=u"DESC"))
+                    order.text = u"▲"
+                else:
+                    a.set(u"href", self.buildQueryOnSameTable(orderColumn=column.name, orderDirection=u"ASC"))
+                    order.text = u"▼"
+            
             col = HTMLIntf.SubElement(colgroup, u"col", span="1")
             if column.width is not None:
                 col.set(u"style", u"width: {0};".format(column.width))
-            a = HTMLIntf.SubElement(th, u"a")
-            a.text = column.title
-            columnName = column.stormColumn.name
-            if orderColumn is not column:
-                a.set(u"href", self.buildQueryOnSameTable(orderColumn=columnName, orderDirection=column.defaultSort))
-            else:
-                divOrder = HTMLIntf.Element(u"div", attrib={
-                    u"class": "order"
-                })
-                #th.insert(0, divOrder)
-                divOrder.tail = a.text
-                a.text = u""
-                a.append(divOrder)
-                if orderDirection == u"ASC":
-                    a.set(u"href", self.buildQueryOnSameTable(orderColumn=columnName, orderDirection=u"DESC"))
-                    divOrder.text = u"▲"
-                else:
-                    a.set(u"href", self.buildQueryOnSameTable(orderColumn=columnName, orderDirection=u"ASC"))
-                    divOrder.text = u"▼"
-                    
+            
+        
         tbody = HTMLIntf.SubElement(table, u"tbody")
         
-        resultSet = virtualTable.select()
+        resultSet = virtualTable.select(orderColumn, orderDirection)
         amount = resultSet.count()
-        resultSet.order_by({
-            u"ASC": Asc,
-            u"DESC": Desc
-        }[orderDirection](orderColumn.stormColumn))
         resultSet.config(offset=offset, limit=limit)
-        # TODO: ordering
-        # TODO: proper limiting!
         
         for obj in resultSet:
+            if not isinstance(obj, tuple):
+                id = obj.ID
+                obj = (obj,)
+            else:
+                id = obj[0].ID
             tr = HTMLIntf.SubElement(tbody, u"tr")
             actions = HTMLIntf.SubElement(tr, u"td", attrib={
                 u"class": u"buttons"
             })
-            HTMLIntf.SubElement(actions, u"a", href=self.editItemHref(obj.ID), title=u"Edit / View this row in detail").text = u"E"
-            HTMLIntf.SubElement(actions, u"a", href=self.buildQueryOnSameTable(orderColumn=orderColumn, orderDirection=orderDirection, touch=obj.ID), title=u"Touch this object (set the Modified timestamp to the current time).").text = u"T"
+            HTMLIntf.SubElement(actions, u"a", href=self.editItemHref(id), title=u"Edit / View this row in detail").text = u"E"
+            HTMLIntf.SubElement(actions, u"a", href=self.buildQueryOnSameTable(orderColumn=orderColumn, orderDirection=orderDirection, touch=id), title=u"Touch this object (set the Modified timestamp to the current time).").text = u"T"
             for column in virtualTable.columns:
-                HTMLIntf.SubElement(tr, u"td").text = column.formatter(getattr(obj, column.stormColumn.name))
+                HTMLIntf.SubElement(tr, u"td").text = column.getFormattedValue(obj)
         
         #amount, = list(self.store.execute("SELECT SQL_FOUND_ROWS()"))[0]
         pages = int(amount/limit)
@@ -158,7 +154,7 @@ class AdminTablesResource(HTMLResource):
             pages += 1
         
         for pageNumber in xrange(1,pages+1):
-            a = HTMLIntf.SubElement(HTMLIntf.SubElement(pagesUl, u"li"), u"a", href=self.buildQueryOnSameTable(orderColumn=orderColumn.stormColumn.name, orderDirection=orderDirection, offset=(pageNumber-1)*limit))
+            a = HTMLIntf.SubElement(HTMLIntf.SubElement(pagesUl, u"li"), u"a", href=self.buildQueryOnSameTable(orderColumn=orderColumn.name, orderDirection=orderDirection, offset=(pageNumber-1)*limit))
             a.text = unicode(pageNumber)
             if pageNumber == (offset/limit)+1:
                 a.set(u"class", u"current")

PriyomHTTP/Server/Resources/Admin/Components.py
@@ -38,24 +38,15 @@ from storm.locals import *
 from storm.expr import *
 from libPriyom.TransmissionParser import NodeError
 
+Sortable = 1
+Filterable = 2
+
 class Column(object):
-    @staticmethod
-    def fmtTimestamp(timestamp):
-        return TimeUtils.fromTimestamp(timestamp).strftime(Formatting.priyomdate)
-    
-    @staticmethod
-    def fmtNone(othFormatter, noneValue=u"None"):
-        def fmt_none(value):
-            if value is None:
-                return noneValue
-            else:
-                return othFormatter(value)
-        return fmt_none
-    
-    def __init__(self, stormColumn, title, formatter=None, width=None, defaultSort=u"ASC"):
+    def __init__(self, stormColumn, title, columnName=None, formatter=None, width=None, defaultSort=u"ASC"):
         if formatter is None:
             formatter = self.fmtNone(unicode)
         self.stormColumn = stormColumn
+        self.name = columnName or self.stormColumn.name
         self.title = title
         self.formatter = formatter
         self.width = width
@@ -76,7 +67,118 @@ class Column(object):
     @classmethod
     def Modified(cls, stormClass, defaultSort=u"DESC", **kwargs):
         return cls.Timestamp(getattr(stormClass, "Modified"), "Modified", defaultSort=defaultSort, **kwargs)
+        
+    @classmethod
+    def Reference(cls, stormColumn, title, columnName, **kwargs):
+        return cls(stormColumn, title, columnName=columnName, **kwargs)
+        
+class VirtualColumn(object):
+    flags = frozenset()
     
+    def __init__(self, title, name=None, formatter=None, width=None, defaultSort=u"ASC", **kwargs):
+        super(VirtualColumn, self).__init__(**kwargs)
+        self.title = title
+        self.name = name or title
+        self.formatter = formatter or unicode
+        self.width = width
+        self.defaultSort = defaultSort
+        
+    def __contains__(self, flag):
+        return flag in self.flags
+        
+    def _directionToStorm(self, direction):
+        return {
+            "ASC": Asc,
+            "DESC": Desc,
+        }[direction]
+        
+    def getFormattedValue(self, returnedTuple):
+        return self.formatter(self.getRawValue(returnedTuple))
+    
+    def sortResultSet(self, resultSet, direction):
+        if not Sortable in self:
+            raise TypeError("Cannot sort by {0} column.".format(self.title))
+        return resultSet.order_by(*self.stormSort(direction))
+        
+        
+class StormColumn(VirtualColumn):
+    def __init__(self, title, stormColumn, **kwargs):
+        super(StormColumn, self).__init__(title, **kwargs)
+        self.stormColumn = stormColumn
+        self.flags = frozenset((Sortable,))
+    
+    def stormArgs(self):
+        return ()
+    
+    def stormColumns(self):
+        return ()
+        
+    def stormSortArgs(self):
+        return ()
+        
+    def stormSort(self, direction):
+        return (self._directionToStorm(direction)(self.stormColumn),)
+    
+    def getRawValue(self, returnedTuple):
+        return getattr(returnedTuple[0], self.stormColumn.name)
+
+class ReferenceColumn(VirtualColumn):
+    def __init__(self, title, reference, remoteClass, attributeName, sortingColumns=None, disableFiltering=False, **kwargs):
+        if reference._on_remote:
+            raise ValueError("References being on the remote are not supported!")
+        localClass = reference._cls
+        super(ReferenceColumn, self).__init__(title, **kwargs)
+        self.localClass = localClass
+        self.remoteClass = remoteClass
+        self.reference = reference
+        self.localKey = reference._local_key
+        self.remoteKey = reference._remote_key
+        self.attributeName = attributeName
+        flags = set()
+        if not disableFiltering:
+            flags.add(Filterable)
+        if sortingColumns is None:
+            self.sortingColumns = sortingColumns
+        else:
+            flags.add(Sortable)
+            self.sortingColumns = list(sortingColumns)
+        self.flags = frozenset(flags)
+    
+    def stormArgs(self):
+        return ()
+    
+    def stormColumns(self):
+        return ()
+        
+    def stormSortArgs(self):
+        return (self.localKey == self.remoteKey,)
+    
+    def stormSort(self, direction):
+        if self.sortingColumns is None:
+            return
+        return tuple((self._directionToStorm(direction)(col) for col in self.sortingColumns))
+    
+    def getFilterIterable(self, store):
+        return iter(store.find(self.remoteClass, self.localKey == self.remoteKey).config(distinct=True))
+    
+    def getRawValue(self, returnedTuple):
+        return getattr(returnedTuple[0], self.attributeName)
+
+class IDColumn(StormColumn):
+    def __init__(self, stormClass, title=u"ID", name=u"ID", **kwargs):
+        super(IDColumn, self).__init__(title, stormClass.ID, name=name, **kwargs)
+
+class TimestampColumn(StormColumn):
+    def __init__(self, title, stormColumn, formatter=None, **kwargs):
+        super(TimestampColumn, self).__init__(title, stormColumn, formatter=(formatter or Formatting.Formatters.catchNone(Formatting.Formatters.Timestamp())), **kwargs)
+        
+class CreatedColumn(TimestampColumn):
+    def __init__(self, stormClass, title=u"Created", **kwargs):
+        super(CreatedColumn, self).__init__(title, stormClass.Created, defaultSort=u"DESC", **kwargs)
+        
+class ModifiedColumn(TimestampColumn):
+    def __init__(self, stormClass, title=u"Modified", **kwargs):
+        super(ModifiedColumn, self).__init__(title, stormClass.Modified, defaultSort=u"DESC", **kwargs)
 
 class Component(object):
     def __init__(self, model=None, **kwargs):
@@ -722,22 +824,32 @@ class VirtualTable(ParentComponent):
         if len(self.columns) == 0:
             raise ValueError(u"Must have a list of columns")
         
-        self.stormColumns = tuple(column.stormColumn for column in self.columns)
+        #self.stormColumns = tuple(column.stormColumn for column in self.columns)
         self.columns = tuple(self.columns)
-        self.columnMap = dict(((column.stormColumn.name, column) for column in self.columns))
+        self.columnMap = dict(((column.name, column) for column in self.columns))
         super(VirtualTable, self).__init__(None, *args, **kwargs)
     
     #def toTree(self, parent):
     #    li = HTMLIntf.SubElement(parent, u"li")
     #    HTMLIntf.SubElement(li, u"a", href=u"../tables/{0}".format(self.name), title=self.description).text = self.name
         
-    def select(self):
+    def select(self, sortColumn, sortDirection):
         store = self.store
         where = self.where
+        args = list(
+            itertools.chain(
+                itertools.chain(*(column.stormArgs() for column in self.columns)),
+                sortColumn.stormSortArgs()
+            )
+        )
         if where is not None:
-            resultSet = store.find(self.cls, where)
-        else:
-            resultSet = store.find(self.cls)
+            args.append(where)
+        resultSet = store.find(self.cls, *args)
+        sortColumn.sortResultSet(resultSet, sortDirection)
+        #if where is not None:
+        #    resultSet = store.find(self.cls, where)
+        #else:
+        #    resultSet = store.find(self.cls)
         return resultSet
         
 class TableComponent(Component):

PriyomHTTP/Server/Resources/Admin/UITree.py
@@ -26,7 +26,7 @@ authors:
 """
 from storm.locals import *
 from libPriyom import *
-from Components import Input, TextArea, Select, SelectStormObject, VirtualTable, Table, TableGroup, IDTableGroup, Timestamp, CheckBox, ForeignInput, TransmissionContents, BroadcastFrequencies, Column
+from Components import Input, TextArea, Select, SelectStormObject, VirtualTable, Table, TableGroup, IDTableGroup, Timestamp, CheckBox, ForeignInput, TransmissionContents, BroadcastFrequencies, StormColumn, ReferenceColumn, IDColumn, TimestampColumn, CreatedColumn, ModifiedColumn
 
 
 """u"schedules": VirtualTable(u"Schedule database", Schedule, 
@@ -116,12 +116,12 @@ virtualTables = {
         ),
         description=u"Shortwave station registry",
         columns=(
-            Column.ID(Station),
-            Column.Created(Station),
-            Column.Modified(Station),
-            Column(Station.EnigmaIdentifier, "Enigma ID", width="8em"),
-            Column(Station.PriyomIdentifier, "Priyom ID", width="8em"),
-            Column(Station.Nickname, "Nickname")
+            IDColumn(Station),
+            CreatedColumn(Station),
+            ModifiedColumn(Station),
+            StormColumn(u"Enigma ID", Station.EnigmaIdentifier, width="8em"),
+            StormColumn(u"Priyom ID", Station.PriyomIdentifier, width="8em"),
+            StormColumn(u"Nickname", Station.Nickname)
         )
         #relatedTables=(
         #    ReferencingVirtualTable(u"broadcasts", match=Broadcast.Station),
@@ -183,12 +183,17 @@ virtualTables = {
         ),
         description=u"Broadcast database",
         columns=(
-            Column.ID(Broadcast),
-            Column.Created(Broadcast),
-            Column.Modified(Broadcast),
-            Column(Broadcast.Type, u"Type"),
-            Column.Timestamp(Broadcast.BroadcastStart, u"Start time"),
-            Column.Timestamp(Broadcast.BroadcastEnd, u"End time")
+            IDColumn(Broadcast),
+            CreatedColumn(Broadcast),
+            ModifiedColumn(Broadcast),
+            TimestampColumn(u"Start time", Broadcast.BroadcastStart),
+            TimestampColumn(u"End time", Broadcast.BroadcastEnd),
+            StormColumn(u"Type", Broadcast.Type),
+            ReferenceColumn(u"Station", Broadcast.Station, Station, u"Station", sortingColumns=(
+                Station.EnigmaIdentifier,
+                Station.PriyomIdentifier,
+                Station.Nickname
+            )),
         )
         #relatedTables=(
         #    ReferenedVirtualTable(u"stations", match=Broadcast.Station),
@@ -233,11 +238,11 @@ virtualTables = {
         ),
         description=u"Transmission database",
         columns=(
-            Column.ID(Transmission),
-            Column.Created(Transmission),
-            Column.Modified(Transmission),
-            Column.Timestamp(Transmission.Timestamp, u"Timestamp"),
-            Column(Transmission.Callsign, u"Callsign")
+            IDColumn(Transmission),
+            CreatedColumn(Transmission),
+            ModifiedColumn(Transmission),
+            TimestampColumn(u"Timestamp", Transmission.Timestamp),
+            StormColumn(u"Callsign", Transmission.Callsign)
         )
     )
 }

libPriyom/Formatting.py
@@ -25,10 +25,9 @@ authors:
     Jonas Wielicki <j.wielicki@sotecware.net>
 """
 priyomdate = "%Y-%m-%dT%H:%M:%S"
-from Types import Typecasts, WrapFunction
-from Helpers import TimeUtils
+from libPriyom.Helpers import TimeUtils
 
-class Formatter(object):
+class Formatters(object):
     @classmethod
     def catchNone(cls, othFormatter, noneValue=u"None"):
         def catch_none(value):

commit 1c92b959f4feca50614cb78bbf2c321265afd960

* Fixed Priyom API Exception: AttributeError: 'NoneType' object has no attribute 'stormSortArgs'   This fixes a regression introduced by 557a0e5cde689538b6fdd8057c88404fdaf4c59f

PriyomHTTP/Server/Resources/Admin/Tables.py
@@ -143,7 +143,7 @@ class AdminTablesResource(HTMLResource):
                 u"class": u"buttons"
             })
             HTMLIntf.SubElement(actions, u"a", href=self.editItemHref(virtualTable.name, id), title=u"Edit / View this row in detail").text = u"E"
-            HTMLIntf.SubElement(actions, u"a", href=self.buildQueryOnSameTable(orderColumn=orderColumn, orderDirection=orderDirection, touch=id), title=u"Touch this object (set the Modified timestamp to the current time).").text = u"T"
+            HTMLIntf.SubElement(actions, u"a", href=self.buildQueryOnSameTable(orderColumn=orderColumn.name, orderDirection=orderDirection, touch=id), title=u"Touch this object (set the Modified timestamp to the current time).").text = u"T"
             for column in virtualTable.columns:
                 HTMLIntf.SubElement(tr, u"td").text = column.getFormattedValue(obj)
         