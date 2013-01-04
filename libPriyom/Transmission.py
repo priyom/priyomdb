ece3969b5a9b1202bfc76a89acce40a051985e50
Jonas Wielicki <j.wielicki@sotecware.net>
2013-01-04 13:07:39 +0100

    Raise if incomplete foreign info is encountered while parsing

--- libPriyom/Transmission.py
+++ libPriyom/Transmission.py
@@ -34,7 +34,7 @@ from libPriyom.Foreign import ForeignHelper
 from libPriyom.Broadcast import Broadcast
 from libPriyom.Station import Station
 from libPriyom.TransmissionParser import TransmissionParserNode, TransmissionParserNodeField, NodeError
-    
+
 class Transmission(PriyomBase, XMLIntf.XMLStorm):
     __storm_table__ = "transmissions"
     ID = Int(primary = True)
@@ -45,12 +45,12 @@ class Transmission(PriyomBase, XMLIntf.XMLStorm):
     ClassID = Int()
     RecordingURL = Unicode()
     Remarks = Unicode()
-    
+
     xmlMapping = {
         u"Recording": "RecordingURL",
         u"Remarks": "Remarks"
     }
-    
+
     def updateBlocks(self):
         blocks = []
         store = Store.of(self)
@@ -59,49 +59,49 @@ class Transmission(PriyomBase, XMLIntf.XMLStorm):
                 blocks.append(block)
         blocks.sort(cmp=lambda x,y: cmp(x.Order, y.Order))
         self._blocks = blocks
-    
+
     def __init__(self):
         super(Transmission, self).__init__()
         self.ForeignCallsign = None
         self._blocks = None
         self._blockUpdate = None
-    
+
     def __storm_invalidated__(self):
         self.ForeignCallsign = ForeignHelper(self, "Callsign")
-    
+
     def __storm_loaded__(self):
         self.ForeignCallsign = ForeignHelper(self, "Callsign")
-        
+
     def _loadCallsign(self, element, context):
         if self.ForeignCallsign is None:
             self.ForeignCallsign = ForeignHelper(self, "Callsign")
-        
+
         lang = element.get(u"lang")
         if lang is not None:
             self.ForeignCallsign.Value = unicode(element.text)
             self.ForeignCallsign.LangCode = unicode(lang)
         else:
             self.Callsign = unicode(element.text)
-            
+
     def _loadBroadcastID(self, element, context):
         self.Broadcast = context.resolveId(Broadcast, int(element.text))
-        
+
     def _loadClassID(self, element, context):
         self.Class = context.resolveId(TransmissionClass, int(element.text))
-        
+
     """
-        Note that loading the contents is, in contrast to most other 
+        Note that loading the contents is, in contrast to most other
         import operations, replacing instead of merging.
     """
     def _loadContents(self, node, context):
         store = Store.of(self)
         for block in self.blocks:
             store.remove(block)
-        
+
         if self.Class is None:
             print("Invalid class id: %d" % (self.ClassID))
             return False
-        
+
         for group in node.iterfind("{{{0}}}group".format(XMLIntf.importNamespace)):
             table = store.find(TransmissionTable, TransmissionTable.TableName == group.getAttribute(u"name")).any()
             if table is None:
@@ -109,7 +109,7 @@ class Transmission(PriyomBase, XMLIntf.XMLStorm):
                 return False
             block = table.PythonClass(store)
             block.fromDom(group, context)
-        
+
     def _metadataToDom(self, parentNode):
         XMLIntf.appendTextElements(parentNode,
             (
@@ -126,17 +126,17 @@ class Transmission(PriyomBase, XMLIntf.XMLStorm):
                 (u"Remarks", self.Remarks)
             )
         )
-        
-    
+
+
     def toDom(self, parentNode, flags=None):
         transmission = XMLIntf.SubElement(parentNode, u"transmission")
         XMLIntf.SubElement(transmission, u"ID").text = unicode(self.ID)
         self._metadataToDom(transmission)
-        
+
         contents = XMLIntf.SubElement(transmission, u"Contents")
         for block in self.blocks:
             block.toDom(contents)
-        
+
     def loadElement(self, tag, element, context):
         try:
             {
@@ -147,36 +147,36 @@ class Transmission(PriyomBase, XMLIntf.XMLStorm):
             }[tag](element, context)
         except KeyError:
             pass
-    
+
     def clear(self):
         store = Store.of(self)
         for table in list(self.Class.tables):
             for block in list(store.find(table.PythonClass, table.PythonClass.TransmissionID == self.ID)):
                 block.deleteForeignSupplements()
                 store.remove(block)
-                
+
     def setParsedContents(self, contents):
         self.clear()
         store = Store.of(self)
-        for order, rowData in itertools.izip(xrange(len(contents)), contents):
+        for order, rowData in enumerate(contents):
             tableClass = rowData[0].PythonClass
             contentDict = rowData[1]
-            
+
             row = tableClass(store)
             row.Transmission = self
             row.Order = order
-            
+
             for key, (value, foreign) in contentDict.iteritems():
                 setattr(row, key, value)
                 if foreign is not None:
                     supplement = row.supplements[key]
                     supplement.LangCode = foreign[0]
                     supplement.Value = foreign[1]
-    
+
     @property
     def Contents(self):
         return u" ".join((unicode(block) for block in self.blocks))
-    
+
     @Contents.setter
     def Contents(self, value):
         try:
@@ -185,11 +185,11 @@ class Transmission(PriyomBase, XMLIntf.XMLStorm):
             raise ValueError(unicode(e))
         self.setParsedContents(parsed)
         self.touch()
-    
+
     @Contents.deleter
     def Contents(self):
         self.clear()
-        
+
     def __unicode__(self):
         return u"Transmission with callsign {0} and {1} segments".format(self.Callsign, len(self.blocks))
 
@@ -206,34 +206,34 @@ class Transmission(PriyomBase, XMLIntf.XMLStorm):
 class TransmissionClassBase(object):
     def __getitem__(self, index):
         return getattr(self, self.fields[index].FieldName)
-    
+
     def __setitem__(self, index, value):
         return setattr(self, self.fields[index].FieldName, value)
-        
+
     def __len__(self):
         return len(self.fields)
-        
+
     def __iter__(self):
         return ((field, getattr(self, field.FieldName)) for field in self.fields)
-        
+
     def initSupplements(self):
         self.supplements = {}
         for field in self.fields:
             self.supplements[field.FieldName] = ForeignHelper(self, field.FieldName)
-        
+
     def __init__(self, store):
         store.add(self)
         self.initSupplements()
-        
+
     def __storm_loaded__(self):
         self.initSupplements()
-        
+
     def toDom(self, parentNode):
         group = XMLIntf.SubElement(parentNode, u"group", {
             u"class": self.TransmissionTable.XMLGroupClass,
             u"name": self.TransmissionTable.TableName
         })
-        
+
         for (field, value) in self:
             kind = field.Kind
             XMLIntf.SubElement(group, u"item", {
@@ -243,7 +243,7 @@ class TransmissionClassBase(object):
             node = supplement.toDom(group, u"item", {
                 u"class": kind
             })
-        
+
     def fromDom(self, element, context):
         fields = iter((field for field in self.fields))
         field = None
@@ -259,11 +259,11 @@ class TransmissionClassBase(object):
                 supplement = self.supplements[field.FieldName]
                 supplement.Value = unicode(item.text)
                 supplement.LangCode = unicode(langCode)
-    
+
     def deleteForeignSupplements(self):
         for supplement in self.supplements.itervalues():
             supplement.removeSupplement()
-        
+
     def formatField(self, fieldName):
         value = getattr(self, fieldName)
         supplement = self.supplements[fieldName]
@@ -271,7 +271,7 @@ class TransmissionClassBase(object):
             return u"{0}/{1}:{2}".format(value, supplement.Value, supplement.LangCode)
         else:
             return u"{0}".format(value)
-    
+
     def __unicode__(self):
         return u" ".join((self.formatField(field.FieldName) for field in self.fields))
 
@@ -302,7 +302,7 @@ class TransmissionTableField(object):
         "other": "other"
     })
     MaxLength = Int()
-    
+
     def toDom(self, parentNode, flags = None):
         fieldNode = XMLIntf.SubElement(parentNode, u"field")
         XMLIntf.appendTextElements(fieldNode, (
@@ -319,10 +319,10 @@ class TransmissionTable(object):
     DisplayName = Unicode()
     XMLGroupClass = Unicode()
     Fields = ReferenceSet(ID, TransmissionTableField.TransmissionTableID)
-    
+
     def __storm_loaded__(self):
         self.PythonClass = NewTransmissionClass(self)
-        
+
     def toDom(self, parentNode, flags = None):
         tableNode = XMLIntf.SubElement(parentNode, u"table")
         XMLIntf.appendTextElements(tableNode, (
@@ -330,7 +330,7 @@ class TransmissionTable(object):
             ("DisplayName", self.DisplayName),
             ("XMLGroupClass", self.XMLGroupClass)
         ))
-        
+
         fieldsNode = XMLIntf.SubElement(tableNode, u"fields")
         for field in self.Fields:
             field.toDom(fieldsNode, flags)
@@ -341,26 +341,39 @@ class TransmissionClass(PriyomBase):
     DisplayName = Unicode()
     RootParserNodeID = Int()
     RootParserNode = Reference(RootParserNodeID, TransmissionParserNode.ID)
-    
+
     def __storm_loaded__(self):
         self.tables = [table for table in self.Tables]
-        
+
     def toDom(self, parentNode, flags = None):
         classNode = XMLIntf.SubElement(parentNode, u"transmission-class")
         XMLIntf.appendTextElements(classNode, (
             (u"ID", unicode(self.ID)),
             (u"DisplayName", self.DisplayName)
         ))
-        
+
         tablesNode = XMLIntf.SubElement(classNode, u"tables")
         for table in self.Tables:
             table.toDom(tablesNode, flags)
-        
+
     def parseNode(self, node, s):
         expr = node.getExpression()
         if node.Table is not None:
             for match in expr.finditer(s):
-                yield (node.Table, dict(((field.FieldName, (match.group(field.Group+1), None if field.ForeignGroup is None or field.ForeignLangGroup is None else (match.group(field.ForeignLangGroup+1), match.group(field.ForeignGroup+1)))) for field in node.Fields)))
+                d = {}
+                for field in node.Fields:
+                    contents = match.group(field.Group+1)
+                    if field.ForeignGroup is not None and field.ForeignLangGroup is not None:
+                        foreignLangCode = match.group(field.ForeignLangGroup+1)
+                        foreignContents = match.group(field.ForeignGroup+1)
+                        if (not foreignContents) ^ (not foreignLangCode):
+                            raise ValueError("Foreign contents or lang code given without the respective other")
+                    else:
+                        foreignLangCode = None
+                        foreignContents = None
+
+                    d[field.FieldName] = (contents, (foreignLangCode, foreignContents))
+                yield (node.Table, d)
         else:
             match = expr.match(s)
             if match is None:
@@ -371,7 +384,7 @@ class TransmissionClass(PriyomBase):
                     raise NodeError("Malformed node: ParentGroup is None at child node #{0}".format(node.ID))
                 for item in self.parseNode(child, groups[child.ParentGroup]):
                     yield item
-        
+
     def parsePlainText(self, s):
         if self.RootParserNode is None:
             raise NodeError("No parser assigned.")
@@ -383,16 +396,16 @@ class TransmissionClass(PriyomBase):
             raise ValueError("Root match failed")
         items = list()
         groups = match.groups()
-        
+
         for child in Store.of(node).find(TransmissionParserNode, TransmissionParserNode.ParentID == node.ID).order_by(Asc(TransmissionParserNode.ParentGroup)):
             if child.ParentGroup is None:
                 raise NodeError("Malformed node: ParentGroup is None at child node #{0}".format(node.ID))
             items.extend(self.parseNode(child, groups[child.ParentGroup]))
         return items
-        
+
     def __unicode__(self):
         return self.DisplayName
-        
+
 class TransmissionClassTable(object):
     __storm_table__ = "transmissionClassTables"
     __storm_primary__ = "ClassID", "TableID"
